6622e8e1ae5f1c1473e3c5b0fa3e02598e192915
premkumar <kumarprem75715@gmail.com>
2025-05-20 10:19:00 +0530

    conference_name, ststus comes under single object

--- src/model/file.model.js
+++ src/model/file.model.js
@@ -2,16 +2,19 @@ const mongoose = require("mongoose");
 
 
 const fileschema = new mongoose.Schema({
-    Title: { type: String, required: true },
-    Author_Mail: { type: String, required: true},
-    Conference_Name: { type: String, required: true },
-    Decision_With_Comments: { type: String, required: true }
+  Title: { type: String, required: true },
+  Author_Mail: { type: String, required: true },
+  Conference: {
+    Conference_Name: { type: String},
+    Decision_With_Commends:{type:String},
+  },
 });
 
 const file = mongoose.model("Excel", fileschema); 
 
 const fileModel = {
     checkTitleExist: async (payload) => {
+        console.log(payload);
         
         const { Title,Author_Mail,Conference_Name,Decision_With_Commends} = payload;
         const exist = await file.findOne({Title});
@@ -19,7 +22,7 @@ const fileModel = {
             return exist;
         }
         else {
-            return await file.insertMany({ Title:Title, Author_Mail:Author_Mail, Conference_Name:Conference_Name, Decision_With_Comments:Decision_With_Commends });
+            return await file.insertMany({ Title: Title, Author_Mail: Author_Mail, Conference: { Conference_Name:Conference_Name, Decision_With_Commends: Decision_With_Commends } });
         }
     },
 
