8719b4ae8b9e48294a5276f6c24f57f7b9415084
premkumar <kumarprem75715@gmail.com>
2025-05-21 13:59:20 +0530

    update and create functions done

--- src/model/file.model.js
+++ src/model/file.model.js
@@ -3,49 +3,93 @@ const mongoose = require("mongoose");
 
 const fileschema = new mongoose.Schema({
   Title: { type: String },
-    Author_Mail: {Author_Mail:{ type: String }},
-  Conference: {
+    Author_Mail: [{Author_Mail:{ type: String }}],
+  Conference:[ {
     Conference_Name: { type: String},
     Decision_With_Commends:{type:String},
-  },
+  },]
 });
 
 const file = mongoose.model("Excel", fileschema); 
 
 const fileModel = {
-    checkTitleExist: async (payload) => {
-        const { Title, Author_Mail, Conference_Name, Decision_With_Commends } = payload;
-        console.log(payload);
+  checkTitleExist: async (payload) => {
+    const { Title, Author_Mail, Conference_Name, Decision_With_Commends } =
+      payload;
+
+    const existingFile = await file.findOne({ Title });
+
+    if (existingFile) {
+      
+      const authorExists = existingFile.Author_Mail.some(
+        (author) => author.Author_Mail === Author_Mail
+      );
+
+      if (authorExists) {
         
-        const exist = await file.findOne({Title});
-        if (exist) {
-            return exist;
+        const conferenceIndex = existingFile.Conference.findIndex(
+          (conf) => conf.Conference_Name === Conference_Name
+        );
+
+        if (conferenceIndex !== -1) {
+        
+          existingFile.Conference[conferenceIndex].Decision_With_Commends =
+            Decision_With_Commends;
+        } else {
+        
+          existingFile.Conference.push({
+            Conference_Name,
+            Decision_With_Commends,
+          });
         }
-        return await file.insertMany([{
-            Title: Title,
-            Author_Mail: { Author_Mail:Author_Mail},
-            Conference: { Conference_Name: Conference_Name, Decision_With_Commends: Decision_With_Commends }
-        }]);
+      } else {
         
-        
-    },
+        existingFile.Author_Mail.push({ Author_Mail });
+         
+        const conferenceIndex = existingFile.Conference.findIndex(
+          (conf) => conf.Conference_Name === Conference_Name
+        );
 
-    createField: async(payload) => {
-        await fileModel.checkTitleExist(payload);
-    },
-
-
-    showFile: async (payload) => {
-        const { Title } = payload;
-        try {
-            const response = await file.find({ Title:Title });
-            return response;
-        } catch (error) {
-            return new Error("erro validating title")
+        if (conferenceIndex !== -1) {
+          existingFile.Conference[conferenceIndex].Decision_With_Commends =
+            Decision_With_Commends;
+        } else {
+          existingFile.Conference.push({
+            Conference_Name,
+            Decision_With_Commends,
+          });
         }
-        
-    },
+      }
 
-}
+    
+      await existingFile.save();
+    } else {
+      await file.insertMany([{
+        Title,
+        Author_Mail: [{ Author_Mail }],
+        Conference: [
+          {
+            Conference_Name,
+            Decision_With_Commends,
+          },
+        ],
+      }]);
+    }
+  },
+
+  createField: async (payload) => {
+    await fileModel.checkTitleExist(payload);
+  },
+
+  showFile: async (payload) => {
+    const { Title } = payload;
+    try {
+      const response = await file.find({ Title: Title });
+      return response;
+    } catch (error) {
+      return new Error("erro validating title");
+    }
+  },
+};
 
 module.exports = fileModel;
