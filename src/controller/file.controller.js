b7d680ed6e2710be87d7875631ce73f348fab0d8
premkumar <kumarprem75715@gmail.com>
2025-05-23 12:50:09 +0530

    pagination added also removed blank spaces in titles

--- src/controller/file.controller.js
+++ src/controller/file.controller.js
@@ -4,7 +4,10 @@ const fileModel = require("../model/file.model");
 const fileController = {
   createFile: async (req, res) => {
     const file = req.file.buffer;
-
+    const page_num = req.query.page || 1;
+    function normalizeTitle(title) {
+      return title?.replace(/\s+/g, " ").trim().toLowerCase();
+    }
     try {
       const workbook = XLSX.read(file, { type: "buffer" });
       const sheetName = workbook.SheetNames[0];
@@ -17,7 +20,7 @@ const fileController = {
         for (const item of data) {
           try {
             const payload = {
-              Title: item.Title,
+              Title: normalizeTitle(item.Title),
               Author_Mail: item.Author_Mail,
               Conference_Name: item.Conference_Name,
               Decision_With_Commends: item.Decision_With_Commends,
@@ -25,13 +28,20 @@ const fileController = {
             
             const responses = await fileModel.createField(payload);
             response.push(responses);
+            
           } catch (error) {
             console.log("error uploading files!" + error);
           }
         }
-
+        //pagination code 
+        const page = page_num;
+        const page_size = 25;
+        const start_index = (page - 1) * page_size;
+        const end_index = start_index + page_size;
+        const total_page = Math.ceil(response.length / page_size);
+        const paginated_data = response.slice(start_index, end_index);
         return res.status(201).json({
-          data: response,
+          page,total_page,response:paginated_data,
           message: "file uploaded to database successfully!",
         });
       }
@@ -41,16 +51,23 @@ const fileController = {
   },
 
   getFile: async (req, res) => {
+    const page_num = req.query.page || 1;
         try {
           const response = await fileModel.getFile();
-          res.status(200).json({ data: response, message: "files fetched successfully" });
+          const page = page_num;
+          const page_size = 25;
+          const start_index = (page - 1) * page_size;
+          const end_index = start_index + page_size;
+          const total_page = Math.ceil(response.length / page_size);
+          const paginated_data = response.slice(start_index, end_index);
+          res.status(200).json({ page,total_page,data:paginated_data, message: "files fetched successfully" });
         } catch (error) {
           res.status(400).json({ message: "No records found!" });
         }
   },
   
   searchTitle: async(req,res) => {
-    const Title = req.query.Title;
+    const Title = req.query.Title.toLowerCase();
     try {
       const response = await fileModel.searchTitle(Title);
       res.status(200).json({data:response,message:"Title matched files fetched successfully!"})
